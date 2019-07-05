0ef53c464a9cd71e3ea9df947922e9203d83a674
DaCoder08 <40702668+DaCoder08@users.noreply.github.com>
2019-07-05 13:42:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+var routes = require('./routes.js');
 
 // Automatically sets view engine and adds dot notation to app.render
 app.use(require('express-edge'));
@@ -8,5 +9,7 @@ app.set('views', `${__dirname}/Views`);
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.use(routes)
 
 app.listen(3000);
